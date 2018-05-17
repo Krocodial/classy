a2d641ad4a95b35a325f3eda45d0d4b17bb9aa38
krocodial <xxkraaklxx@gmail.com>
2018-05-17 14:03:53 -0700

    cleaning

--- classy/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import Permission
-# Register your models here.
-
-from .models import classification
-
-admin.site.register(Permission)
-#admin.site.register(classification)
-
