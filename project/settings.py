b3acc36eef49a8a8a8b808a06a6a7f20ed6721ab
LKRAAK <Louis.kraak@gov.bc.ca>
2018-12-31 11:51:12 -0800

    static files

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 FILE_UPLOAD_PERMISSIONS = 0o600
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'conf/html/')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
